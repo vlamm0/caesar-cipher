2125d7d0e52c30fe8466b7007f4be1788d552bd3
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-28 11:39:59 -0400

    Encrypt a message using a custom shift factor

--- main.rb
+++ main.rb
@@ -1,21 +1,41 @@
 ALPHA = "abcdefghijklmnopqrstuvwxyz".split("")
 
-#works as an encryption function
+
+# determines if the new letter should be capitalized or not
+def capitalization(oldLetter, newLetter)
+  if oldLetter == oldLetter.upcase
+    newLetter.upcase
+  else
+    newLetter
+  end
+end
+
+
+# works as an encryption function using users message and shift factor
 def encrypt(str, shiftFactor)
   str.split("").reduce("") { |newStr, letter|
-    if ALPHA.include?(letter)
-      newIndex = ALPHA.index(letter) + shiftFactor
+    if ALPHA.include?(letter.downcase)
+      newIndex = ALPHA.index(letter.downcase) + shiftFactor
       if newIndex > 25
         newIndex -= 26
+        
       end
-      newStr += ALPHA[newIndex]
+      newStr += capitalization(letter, ALPHA[newIndex])
     else
       newStr += letter
     end
   }
 end
-puts "Enter in a message for encryption"
-string = gets.chomp
-puts "Enter a shift factor"
-shift = gets.to_i
-puts encrypt(string, shift)
+
+# gets message (and shift factor) from user for encryption
+def getMessage()
+  puts "Enter in a message for encryption"
+  string = gets.chomp
+  puts "Enter a shift factor"
+  shift = gets.to_i
+  puts "\n\n***EnCrYpTeD mEsSaGe***"
+  puts encrypt(string, shift)
+end
+
+# main code
+getMessage()
