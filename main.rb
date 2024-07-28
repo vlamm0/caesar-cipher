063d9bd2172bba2d729be373cde9222d1420a5b4
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-27 23:12:15 -0400

    Get message from user and return encrypted

--- main.rb
+++ main.rb
@@ -1,20 +1,21 @@
-alpha = "abcdefghijklmnopqrstuvwxyz".split("")
-str = "what a string"
-strArr = str.split("")
-#p strArr
+ALPHA = "abcdefghijklmnopqrstuvwxyz".split("")
 
-newStr = ""
-
-strArr.each { |letter|
-  if alpha.include?(letter)
-    newIndex = alpha.index(letter) + 5
-    if newIndex > 25
-      newIndex -= 26
+#works as an encryption function
+def encrypt(str, shiftFactor)
+  str.split("").reduce("") { |newStr, letter|
+    if ALPHA.include?(letter)
+      newIndex = ALPHA.index(letter) + shiftFactor
+      if newIndex > 25
+        newIndex -= 26
+      end
+      newStr += ALPHA[newIndex]
+    else
+      newStr += letter
     end
-    newStr += alpha[newIndex]
-  else
-    newStr += letter
-  end
-
-}
-puts newStr
+  }
+end
+puts "Enter in a message for encryption"
+string = gets.chomp
+puts "Enter a shift factor"
+shift = gets.to_i
+puts encrypt(string, shift)
